305333155a89b6e71049af952c4f2ce7e2c17c5b
ariktadas144 <ariktadas144@gmail.com>
2025-08-03 06:14:43 +0530

    Update App.jsx

--- clients/src/App.jsx
+++ clients/src/App.jsx
@@ -1,8 +1,14 @@
-import Routes from './Routes';
+// App.jsx
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Desktop2 from './pages/Desktop2';
 
 function App() {
   return (
-        <Routes />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/desktop2" element={<Desktop2 />} />
+    </Routes>
   );
 }
 
